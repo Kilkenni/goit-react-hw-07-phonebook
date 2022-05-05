030c3dba5d29d480292dadc6756bc6150b95a267
Kilkenni <hpittman89@outlook.com>
2022-05-05 21:13:54 +0300

    typo in package.json

--- src/redux/contacts/items.js
+++ src/redux/contacts/items.js
@@ -1,5 +1,5 @@
 import { /*createAction, createReducer,*/ createSlice } from "@reduxjs/toolkit";
-//import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import { nanoid } from "nanoid";
 import { toast } from "react-toastify";
 
@@ -42,35 +42,19 @@ export const contactItemsSlice = createSlice({
   },
 });
 
-//made via "Map" notation
-/*const itemsReducer = createReducer([], {
-  [addContact]: (items = [], action) => {
-    const { name: newName } = action.payload; //destruct new contact from payload
-    const normalizedNewName = newName.toLowerCase(); //check if the person already exists in contacts
-
-    if (items.some( (contact) => {
-      return contact.name.toLowerCase() === normalizedNewName;
-    })) {
-      toast.error(`${newName} is already in contacts.`, {autoClose: 2000}); //new error message with react-toastify
-      //alert(`${newName} is already in contacts.`);
-      return items;
-    }
-
-    return [...items, action.payload]
-  }, 
-
-  [deleteContact.type]: (items = [], action) => {
-    return items.filter((contact) => {
-      return contact.id !== action.payload;
-    });
-  }, //action.type not really necessary as toString() for action created via createAction already returns type
-});*/
-
 export const selectItems = (state) => {
   return state.contacts.items;
 }
 
-// const fetchContacts = createAsyncThunk();
+export const fetchContacts = createAsyncThunk(
+  
+);
+
+export const getContacts = () => {
+  return (dispatch) => {
+
+  }
+}
 
 export const { addContact, deleteContact } = contactItemsSlice.actions;
 
